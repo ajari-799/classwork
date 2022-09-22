e3c65a144f240a05aa6915545ac85609ec3648a8
ajari-799 <114122236+ajari-799@users.noreply.github.com>
2022-09-22 12:24:14 -0700

    love msg

---
+++ 8_29_2022.py
@@ -0,0 +1,22 @@
+# x = int(input("Face number:\n"))
+# print(" ///|\\\\\\")
+# print("/ 0   0 \\")
+# print("|       |\n" * x, end='')
+# print("|   I   |")
+# print("|       |\n" * x, end='')
+# print("| \___/ |")
+# print(" \_____/")
+
+
+# d = 'hi there' * 'hi there'
+# print(d)
+
+jdjfdjv
+dsfm,fmf
+
+# I love you
+# how are you
+
+x = float(input("Enter circle diameter:"))
+y = float(3.14*(x/2)**2)
+print("Area of circle with diameter", x, "is", y)
